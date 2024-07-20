606181f9f22ed5d3993715244ef780a6879d57e5
mprody <mariorobertoh99@gmail.com>
2024-07-20 01:31:24 -0600

    creacion del repositorio

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { build, defineConfig } from "vite";
+import * as glob from "glob";
+import {ViteMinifyPlugin} from 'vite-plugin-minify';
+import htmlPurge from 'vite-plugin-purgecss';
+import path,{resolve} from "node:path";
+import handlebars from 'vite-plugin-handlebars';
+
+
+export default defineConfig(
+    {
+        appType:"mpa",
+        base: "/galeria/",
+        build:{
+            rollupOptions:{
+                input:Object.fromEntries(
+                    [
+                        ...glob.sync('./!(dist)/*.html').map(
+                            file=>[
+                                file.slice(0,file.length-path.extname(file).length),
+                                resolve(__dirname,file)
+                            ]
+                        ),
+                        ...glob.sync('./*.html').map(
+                            file=>[
+                                file.slice(0,file.length-path.extname(file).length),
+                                resolve(__dirname,file)
+                            ]
+                        )
+                    ]
+                )
+            }
+        },
+        plugins:[
+            handlebars({
+                partialDirectory:resolve(__dirname,'parciales'),
+            }),
+            htmlPurge({}),
+            ViteMinifyPlugin()
+        ]
+
+    }
+);
